47493f0af477fb9ebfc0154e63c81a4db0c3b235
aleckid77@gmail.com <MyGitHub1>
2023-06-06 19:15:51 +0300

    project init

---
+++ src/test/java/ru/smirnov/restaurant_voting/RestaurantVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.smirnov.restaurant_voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

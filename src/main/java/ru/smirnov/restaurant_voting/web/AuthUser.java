9c6453c6e88b74f90b44fdf0a11c306222bb4711
aleckid77@gmail.com <MyGitHub1>
2023-06-12 21:40:42 +0300

    feat: MatcherFactory feat: to web: ProfileController, UniqueMailValidator

---
+++ src/main/java/ru/smirnov/restaurant_voting/web/AuthUser.java
@@ -0,0 +1,54 @@
+package ru.smirnov.restaurant_voting.web;
+
+import lombok.Getter;
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import ru.smirnov.restaurant_voting.model.Role;
+import ru.smirnov.restaurant_voting.model.User;
+
+import static java.util.Objects.requireNonNull;
+
+@Getter
+public class AuthUser extends org.springframework.security.core.userdetails.User {
+
+    private final User user;
+
+    public AuthUser(@NonNull User user) {
+        super(user.getEmail(), user.getPassword(), user.getRoles());
+        this.user = user;
+    }
+
+    public int id() {
+        return user.id();
+    }
+
+    public static AuthUser safeGet() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null) {
+            return null;
+        }
+        return (auth.getPrincipal() instanceof AuthUser au) ? au : null;
+    }
+
+    public static AuthUser get() {
+        return requireNonNull(safeGet(), "No authorized user found");
+    }
+
+    public static User authUser() {
+        return get().getUser();
+    }
+
+    public static int authId() {
+        return get().id();
+    }
+
+    public boolean hasRole(Role role) {
+        return user.hasRole(role);
+    }
+
+    @Override
+    public String toString() {
+        return "AuthUser:" + id() + '[' + user.getEmail() + ']';
+    }
+}

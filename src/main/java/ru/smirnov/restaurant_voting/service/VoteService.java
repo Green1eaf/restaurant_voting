bbb04a76d2412aab91aa8cb909aaf54a256e470d
aleckid77@gmail.com <MyGitHub1>
2023-06-17 15:45:45 +0300

    feat: vote

---
+++ src/main/java/ru/smirnov/restaurant_voting/service/VoteService.java
@@ -0,0 +1,50 @@
+package ru.smirnov.restaurant_voting.service;
+
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.smirnov.restaurant_voting.error.DataConflictException;
+import ru.smirnov.restaurant_voting.model.User;
+import ru.smirnov.restaurant_voting.model.Vote;
+import ru.smirnov.restaurant_voting.repository.RestaurantRepository;
+import ru.smirnov.restaurant_voting.repository.VoteRepository;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class VoteService {
+    private final VoteRepository repository;
+    private final RestaurantRepository restaurantRepository;
+    private static final LocalTime DEADLINE = LocalTime.of(11, 0);
+
+    @Transactional
+    public Vote createToday(User user, int restaurantId) {
+        LocalDateTime now = LocalDateTime.now();
+        Optional<Vote> dbVote = repository.getByUserIdAndDate(user.id(), now.toLocalDate());
+        dbVote.ifPresent(v -> {
+            throw new DataConflictException("Already has been voted today");
+        });
+        restaurantRepository.checkAvailable(restaurantId);
+        return repository.save(new Vote(null, user, now.toLocalDate(), now.toLocalTime(), restaurantId));
+    }
+
+    @Transactional
+    public void updateToday(User user, int restaurantId, boolean deleteVote) {
+        LocalDateTime now = LocalDateTime.now();
+        if (now.toLocalTime().isAfter(DEADLINE)) {
+            throw new DataConflictException("Deadline for changing vote has been passed");
+        }
+        Optional<Vote> dbVote = repository.getByUserIdAndDate(user.id(), now.toLocalDate());
+        Vote vote = dbVote.orElseThrow(() -> new DataConflictException("You does not voted today"));
+        if (deleteVote) {
+            repository.delete(vote.id());
+        } else {
+            restaurantRepository.checkAvailable(restaurantId);
+            vote.setActualTime(now.toLocalTime());
+            vote.setRestaurantId(restaurantId);
+        }
+    }
+}
--- src/main/java/ru/smirnov/restaurant_voting/web/vote/VoteController.java
+++ src/main/java/ru/smirnov/restaurant_voting/web/vote/VoteController.java
@@ -1,19 +1,23 @@
 package ru.smirnov.restaurant_voting.web.vote;
 
+import io.swagger.v3.oas.annotations.media.Content;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.smirnov.restaurant_voting.model.User;
 import ru.smirnov.restaurant_voting.model.Vote;
 import ru.smirnov.restaurant_voting.repository.VoteRepository;
+import ru.smirnov.restaurant_voting.service.VoteService;
 import ru.smirnov.restaurant_voting.web.AuthUser;
 
+import java.net.URI;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -25,6 +29,7 @@ public class VoteController {
     static final String REST_URL = "/api/votes";
 
     private final VoteRepository repository;
+    private final VoteService service;
 
     @GetMapping
     public List<Vote> getOwns() {
@@ -39,5 +44,35 @@ public class VoteController {
         if (date == null) date = LocalDate.now();
         log.info("getOwnsByDate for userId={} and {}", authId, date);
         return ResponseEntity.of(repository.getByUserIdAndDate(authId, date));
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)// Needed for CSRF protection
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(mediaType = "application/json"))
+    // https://github.com/springdoc/springdoc-openapi/issues/657#issuecomment-625891941
+    public ResponseEntity<Vote> voteToday(@RequestParam int restaurantId, @AuthenticationPrincipal AuthUser authUser) {
+        User user = authUser.getUser();
+        log.info("voteToday for userId={}", user.id());
+        Vote created = service.createToday(user, restaurantId);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/by-date?date={date}")
+                .buildAndExpand(created.getActualDate()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(mediaType = "application/json"))
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void revoteToday(@RequestParam int restaurantId, @AuthenticationPrincipal AuthUser authUser) {
+        User user = authUser.getUser();
+        log.info("re-voteToday for userId={}", user.id());
+        service.updateToday(user, restaurantId, false);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteToday(@AuthenticationPrincipal AuthUser authUser) {
+        User user = authUser.getUser();
+        log.info("deleteToday for userId={}", user.id());
+        service.updateToday(user, 0, true);
     }
 }
